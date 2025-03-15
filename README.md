d3321dbdc9fd3ab13047e39b3724274f31af11fb
lucxsdeveloper <lucasdaniel2270@gmail.com>
2025-03-15 17:25:01 -0300

    commit com projeto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # site-aulavip
-Este repositório é um projeto desenvolvido em uma disciplina de projeto integrador do meu curso de graduação. Nele, eu, junto com outros colegas, desenvolvemos um site para a oferta e procura de professores particulares do estado do Rio de Janeiro. 
+Este repositório é um projeto desenvolvido em uma disciplina de projeto integrador do meu curso de graduação. Nele, eu, junto com outros colegas, desenvolvemos um site para a oferta e procura de professores particulares do estado do Rio de Janeiro.
---
+++ siteDeAula/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de aula</title>
+</head>
+<body>
+    <?php
+        // conexão (servidor, usuário, senha, database)
+        $conn = new mysqli('localhost','usuário','Senha123','classic');
+        $query = INSERT into offices(officeCode,city,pone,addressLine1,addressLine2,state,country,postalCode,territory) VALUES ('18','São Gonçalo','55  ') 
+
+    ?>
+    
+</body>
+</html>
